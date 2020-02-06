9fedfbdba5741114bce5ebbbb49037a1c68016c3
Dumenco <tt-3484@ukr.net>
2020-02-06 10:55:18 +0200

    home-7

--- hw-7/js/task-1.js
+++ hw-7/js/task-1.js
@@ -1,5 +1,10 @@
-const itemsAll =document.querySelectorAll(".item");
-const items = itemsAll.forEach (item => 
-    console.log(`Категорія: ${item.querySelector('h2').textContent}. Кількість елементів: ${item.lastElementChild.children.length}`))
+const category = document.querySelector('#categories');
+console.log(`Всього: ${category.childElementCount} категорії`);
 
-
+const itemsAll = document.querySelectorAll('.item');
+const items = itemsAll.forEach((item) =>
+	console.log(
+		`Категорія: ${item.querySelector('h2').textContent}. Кількість елементів: ${item.lastElementChild.children
+			.length}`
+	)
+);
